2905c31882dc2e10553c6f9842ead5453f6fbf1c
安德航 <bjwltiankong@163.com>
2017-02-08 10:42:34 +0800

    Initial Commit

---
+++ ADCarouselView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADCarouselView
+//
+//  Created by andehang on 2017/2/8.
+//  Copyright © 2017年 andehang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
